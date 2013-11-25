77387b3580c65b3b236b99c037487be0b1c08456
nishacodes <nishabatra27@gmail.com>
2013-11-25 11:53:16 -0500

    scrape all pokemon names and urls and put them into a hash

--- lib/pokemonscraper.rb
+++ lib/pokemonscraper.rb
@@ -1,21 +1,41 @@
 require 'open-uri' 
 require 'nokogiri' 
+require 'debugger'
 
 class PokemonScraper
 	attr_reader :html
 	
 	def initialize (url)
 		download = open(url)
-		@html = Nokogiri::HTML(download)  
+		@html = Nokogiri::HTML(download) 
+		@names
+		@urls 
+		@names_hash = {}
 	end
 
 	def get_pokemon_name
 		name = html.search(".firstHeading").text.split(" ")[0]
-		name 
+		puts name 
 	end
 
+	def get_all_names
+		names = html.search("tr td:nth-child(4) a").collect do |div|
+			div.text
+		end
+		urls = html.search("tr td:nth-child(4) a").collect do |div|
+			"http://bulbapedia.bulbagarden.net/" + div["href"]
+		end 
+		
+	end
+	def create_names_hash
+		names.each do |name|
+			urls.each do |url|
+				names_hash[name] = url
+			end
+		end
+	end
 end
 
-igglybuff = PokemonScraper.new("http://bulbapedia.bulbagarden.net/wiki/Igglybuff_(Pok%C3%A9mon)")
-
-igglybuff.get_pokemon_name
+# ATTEMPT FOR ALL THE NAMES
+pokemon = PokemonScraper.new("http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_index_number_(Generation_V)")
+pokemon.get_all_names
